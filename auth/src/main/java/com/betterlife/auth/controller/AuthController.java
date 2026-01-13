6999ab19bab0197b049322e8b1f197ba39c5386a
hyeongsh <shinhs6384@naver.com>
2026-01-13 15:32:14 +0900

    swagger 상세 설명 추가

--- auth/src/main/java/com/betterlife/auth/controller/AuthController.java
+++ auth/src/main/java/com/betterlife/auth/controller/AuthController.java
@@ -2,8 +2,14 @@ package com.betterlife.auth.controller;
 
 import com.betterlife.auth.dto.*;
 import com.betterlife.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,12 +26,27 @@ public class AuthController {
 //        return ResponseEntity.ok(authService.me(userId));
 //    }
 
+    @Operation(operationId = "register", summary = "회원가입")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "회원가입 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 형식",
+                    content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
+            @ApiResponse(responseCode = "409", description = "중복된 이메일",
+                    content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
+    })
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
         authService.register(request);
-        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입이 완료되었습니다.");
     }
 
+    @Operation(operationId = "login", summary = "login")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "로그인 성공",
+                    content = @Content(schema = @Schema(implementation = ResponseEntity.class))),
+            @ApiResponse(responseCode = "400", description = "잘못된 형식",
+                    content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))),
+    })
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request, HttpServletResponse response) {
         String refresh = authService.login(request);
