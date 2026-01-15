838efab2e3a0384427e63fdb4b53f3cd1d2bbe0a
hyeongsh <shinhs6384@naver.com>
2026-01-15 16:29:33 +0900

    feat: 로그인 성공 통합 테스트 추가

--- auth/src/test/java/com/betterlife/auth/integration/AuthIntegrationTest.java
+++ auth/src/test/java/com/betterlife/auth/integration/AuthIntegrationTest.java
@@ -1,18 +1,22 @@
 package com.betterlife.auth.integration;
 
 import com.betterlife.auth.repository.UserRepository;
+import com.betterlife.auth.util.JwtProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.hamcrest.Matchers;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 
 @SpringBootTest
@@ -23,7 +27,13 @@ public class AuthIntegrationTest extends IntegrationTestBase {
     private MockMvc mockMvc;
 
     @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private JwtProvider jwtProvider;
 
     @BeforeEach
     void beforeEach() {
@@ -57,7 +67,7 @@ public class AuthIntegrationTest extends IntegrationTestBase {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(reqJsonInvalid))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("숫자와 문자")));
+                .andExpect(MockMvcResultMatchers.content().string(containsString("숫자와 문자")));
         assertThat(before).isEqualTo(userRepository.count());
 
         before = userRepository.count();
@@ -65,7 +75,7 @@ public class AuthIntegrationTest extends IntegrationTestBase {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(reqJson))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("회원가입")));
+                .andExpect(MockMvcResultMatchers.content().string(containsString("회원가입")));
         assertThat(before + 1).isEqualTo(userRepository.count());
 
         before = userRepository.count();
@@ -73,7 +83,59 @@ public class AuthIntegrationTest extends IntegrationTestBase {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(reqJson))
                 .andExpect(MockMvcResultMatchers.status().isConflict())
-                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("이미 존재")));
+                .andExpect(MockMvcResultMatchers.content().string(containsString("이미 존재")));
         assertThat(before).isEqualTo(userRepository.count());
+    }
+
+    @Test
+    void login() throws Exception {
+        String email = "sample@test.com";
+        String rawPassword = "AbCd1234";
+        String rawPasswordInvalid = "12345678";
+
+        String reqJson = """
+                {
+                    "name": "test",
+                    "email": "%s",
+                    "password": "%s"
+                }
+                """.formatted(email, rawPassword);
+
+        String reqJsonLogin = """
+                {
+                    "email": "%s",
+                    "password": "%s"
+                }
+                """.formatted(email, rawPassword);
+
+        String reqJsonLoginInvalid = """
+                {
+                    "email": "%s",
+                    "password": "%s"
+                }
+                """.formatted(email, rawPasswordInvalid);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/auth/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(reqJson));
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(reqJsonLogin))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.token").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("test"))
+                .andExpect(MockMvcResultMatchers.header().string("Set-Cookie", containsString("refresh_token=")))
+                .andReturn();
+
+        String setCookie = result.getResponse().getHeader("Set-Cookie");
+        String refreshToken = setCookie.split("refresh_token=")[1].split(";")[0];
+
+        Long userId = jwtProvider.getUserId(refreshToken);
+        String sessionId = jwtProvider.getSessionId(refreshToken);
+
+        String redisKey = "refresh:" + userId + ":" + sessionId;
+
+        assertThat(redisTemplate.hasKey(redisKey)).isTrue();
     }
 }
